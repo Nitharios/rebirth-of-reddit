2b0ef5069c9ab278f40f81fd5bdf6cde5e9a0769
Nathan N <Nitharios@users.noreply.github.com>
2017-09-25 12:34:36 -1000

    Appended titles to divWrapper

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 // jshint esversion:6
+console.log('Sanity Check: Rebirth of Reddit');
 
 /*
 JSON DOC
@@ -6,25 +7,28 @@ JSON DOC
   children:
     Array[0] [{
       data: {
-        preview,
-        thumbnail,
-        title,
-        url
+        preview {
+          images: Array[1]
+          },
+        thumbnail: someLink,
+        title: /r/Some String,
+        url: anotherLink
       }
     }]
 */
 
-console.log('Sanity Check: Rebirth of Reddit');
+// Will store array of data retrieve from API
+let arrayOfAww = [];
+let arrayInAww = {};
+let wrapperDiv = document.getElementById("wrapper");
 
-retrieveAPI('https://www.reddit.com/r/aww.json');
-
+// Need to build a function that grabs the data from the Array in the JSON object
 function requestListener() {
   return function() {
-    let parsedDocument = JSON.parse(this.responseText);
-  
-    console.log(parsedDocument);
-  
-    return parsedDocument;
+    let parsedDocument = JSON.parse(this.responseText).data.children;
+    arrayOfAww = parsedDocument;
+
+    generateDivChildren(arrayOfAww);
   };
 }
 
@@ -34,4 +38,22 @@ function retrieveAPI(url) {
   apiRequest.addEventListener("load", requestListener());
   apiRequest.open("GET", url);
   apiRequest.send();
+
+  return arrayOfAww;
 }
+
+function generateDivChildren(array) {
+  console.log(array);
+  for (let i=0; i<array.length; i++) {
+    let previewDiv = document.createElement('div');
+
+    previewDiv.className = "preview";
+    previewDiv.innerHTML = array[i].data.title;
+    console.log(previewDiv);
+
+    wrapperDiv.appendChild(previewDiv);
+  }
+}
+
+retrieveAPI('https://www.reddit.com/r/aww.json');
+// generateDivChildren(arrayOfAww);
